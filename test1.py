795b28b1e381444bc1f2484929a76c74f84ec32f
Adrian Stankiewicz <AdrianStankiewiczIT@gmail.com>
2023-03-14 23:27:24 +0100

    added first konsultacje scrape

--- test1.py
+++ test1.py
@@ -11,20 +11,22 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Find all the rows in the table
 rows = soup.find_all('tr')
 
-emptyList = []
-
 # Loop through each row and extract the data
 for row in rows:
+
     # Find all the cells in each row
     cells = row.find_all("td")
 
     # check if cells isn't empty
     if bool(cells):
-        print(cells)
-        
-    # Extrac    t the text from each cell
-#    name = cells[0].text.strip()
-#    time = cells[1].text.strip()
-#    room = cells[2].text.strip()
-    # Display the data
-#    print(f"{name}: {time} ({room})")
+        for cell in cells:
+            tag = soup.new_tag("br")
+            tag.string = "\n"
+            cell.replace_with(tag)
+            
+            clearCell = cell.text
+
+            clearCell = clearCell.replace('\t', '')
+            #clearCell = clearCell.replace('\n', '')
+            
+            print(clearCell)
