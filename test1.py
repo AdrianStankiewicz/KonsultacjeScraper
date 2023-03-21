1fd1a0acfb2b7ab577b3e0ed11d935a726478049
Adrian Stankiewicz <AdrianStankiewiczIT@gmail.com>
2023-03-21 14:24:18 +0100

    added capability to scrape all of we

--- test1.py
+++ test1.py
@@ -2,28 +2,32 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-# Make a GET request to the URL
-url = 'https://we.umg.edu.pl/ktm/konsultacje'
-response = requests.get(url)
+# List of URLs to scrape from
+urls = ['https://we.umg.edu.pl/kao/konsultacje',
+       'https://we.umg.edu.pl/kem/konsultacje',
+       'https://we.umg.edu.pl/keo/konsultacje',
+       'https://we.umg.edu.pl/ktm/konsultacje']
 
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# List for data scraped
 data = []
 
-for p in soup.select('tr:has(h3)'):
-    d = {
-        'prof': p.h3.contents[0].strip(),
-        'room': p.h3.contents[-1].string.strip(),
-        'times': []
-    }
+for url in urls:
+    response = requests.get(url)
+    
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    for p in soup.select('tr:has(h3)'):
+        d = {
+            'proffessor': p.h3.contents[0].strip(),
+            'room': p.h3.contents[-1].string.strip(),
+            'times': []
+        }
 
-    for e in p.find_next_siblings('tr'):
-        if e.h3:
-            break
-        if len(e.text.strip()) > 1 and not e.h5:
-            d['times'].append(e.get_text('|',strip=True))
-    data.append(d)
+        for e in p.find_next_siblings('tr'):
+            if e.h3:
+                break
+            if len(e.text.strip()) > 1 and not e.h5:
+                d['times'].append(e.get_text('|',strip=True))
+        data.append(d)
 
 json_object = json.dumps(data, indent = 4, ensure_ascii = False) 
 print(json_object)
